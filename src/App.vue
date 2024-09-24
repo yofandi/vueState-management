37e8c6df7e4fc828ab93cfa59ad696331a6ecfb8
yofandirikiwinata <yofandirikiwinata24@gmail.com>
2024-09-25 00:11:36 +0700

    add: Cara Kerja State Management Dengan Ref

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+const red = ref(0);
+const green = ref(0);
+const blue = ref(0);
+</script>
+<template>
+  <main :style="{ backgroundColor: `rgb(${red}, ${green}, ${blue})` }">
+    <div>
+      <input type="number" placeholder="red" v-model="red" id="" />
+      <input type="number" placeholder="green" v-model="green" id="" />
+      <input type="number" placeholder="blue" v-model="blue" id="" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: rgb(122, 50, 255);
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  display: flex;
+  flex-direction: column;
+}
+</style>
