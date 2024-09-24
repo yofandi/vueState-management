08a6bc73d73cac7010a7d4b071f46119020a9ad9
yofandirikiwinata <yofandirikiwinata24@gmail.com>
2024-09-25 04:48:07 +0700

    add: Perbedaan Menggunakan Ref Dan Reactive

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { reactive, ref } from "vue";
 
-// const red = ref(0);
-// const green = ref(0);
-// const blue = ref(0);
+const red = ref(0);
+const green = ref(0);
+const blue = ref(0);
 
 // const color = ref({
 //   red: 0,
@@ -16,6 +16,27 @@ const color = reactive({
   green: 0,
   blue: 0,
 });
+
+function generatoColor() {
+  // red.value = Math.floor(Math.random() * 255);
+  // green.value = Math.floor(Math.random() * 255);
+  // blue.value = Math.floor(Math.random() * 255);
+
+  color.red = Math.floor(Math.random() * 255);
+  color.green = Math.floor(Math.random() * 255);
+  color.blue = Math.floor(Math.random() * 255);
+}
+
+const refValue = ref("hello");
+const reactiveValue = reactive({
+  greeting: "hello",
+});
+
+refValue.value = true;
+reactiveValue.greeting = false;
+
+console.log(refValue.value);
+console.log(reactiveValue);
 </script>
 <template>
   <main
@@ -27,6 +48,7 @@ const color = reactive({
       <input type="number" placeholder="red" v-model="color.red" id="" />
       <input type="number" placeholder="green" v-model="color.green" id="" />
       <input type="number" placeholder="blue" v-model="color.blue" id="" />
+      <button @click="generatoColor">Generate</button>
     </div>
   </main>
 </template>
